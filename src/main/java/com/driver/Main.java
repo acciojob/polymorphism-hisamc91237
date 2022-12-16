739503ccf789963685691f7fbf73ae7feeab5607
hisamc91237 <hisamc2000@gmail.com>
2022-12-16 12:26:56 +0530

    Message

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
 
+        public int Product(int x,int y){
+            return x*y;
+        }
+        public int Product(int x,int y,int z){
+            return x*y*z;
+        }
+        public double Product(double x,double y){
+            return x*y;
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Product p = new Product();
+
+        p.Product(1,2);
+        p.Product(1,2,3);
+        p.Product(1.2,1.4);
+    }
 }
