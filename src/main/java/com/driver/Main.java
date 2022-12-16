074ffe91fb47ed62c988f47478250fce0a3ab2d4
hisamc91237 <hisamc2000@gmail.com>
2022-12-16 12:57:55 +0530

    Message1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -15,7 +15,6 @@ public class Main {
     }
 
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
         Product p = new Product();
 
         p.Product(1,2);
